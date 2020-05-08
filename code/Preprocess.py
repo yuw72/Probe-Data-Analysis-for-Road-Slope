9707ec6cd76bf8cbe30262181a52b7318027569c
yuw72 <32040841+yuw72@users.noreply.github.com>
2020-05-08 15:33:07 -0500

    Add files via upload

--- code/Preprocess.py
+++ code/Preprocess.py
@@ -18,34 +18,25 @@ def preprocess(df_probe, df_link):
         df_probe.at[idx,'altitude'] = z
 
         idx+=1
-    # t2 = datetime.datetime.now()
-    # print(t2-t1)
-    # for lat, lon, alt in zip(df_probe['latitude'], df_probe['longitude'], df_probe['altitude']):
-    #     print(lat,lon,alt)
-    # idx = 0
-    # for link_shape in df_link['shapeInfo']:
-    #     # if idx>100:
-    #     #     break
-    #     if idx%1000==0:
-    #         print("index: ",idx)
-    #     link_shape = link_shape.split('|')
-    #     newstr = ''
-    #     for i in range(len(link_shape)):
-    #         lat,lon,alt = link_shape[i].split('/')
-    #         if alt=='':
-    #             alt = '0'
-    #         x,y,z=converter(float(lat),float(lon),float(alt))
-    #         newstr += str(x)+"/"+str(y)+"/"+str(z)+"|"
-    #     newstr = newstr[:-1]
-    #     df_link['shapeInfo'][idx] = newstr
-    #     idx+=1  
-    # print(datetime.datetime.now()-t2)
-    # idx = 0
-    # for link_shape in df_link['shapeInfo']:
-    #     if idx>10:
-    #         break
-    #     print(link_shape)
-    #     idx+=1
+  
+    idx = 0
+    for link_shape in df_link['shapeInfo']:
+        # if idx>100:
+        #     break
+        if idx%1000==0:
+            print("index: ",idx)
+        link_shape = link_shape.split('|')
+        newstr = ''
+        for i in range(len(link_shape)):
+            lat,lon,alt = link_shape[i].split('/')
+            if alt=='':
+                alt = '0'
+            x,y,z=converter(float(lat),float(lon),float(alt))
+            newstr += str(x)+"/"+str(y)+"/"+str(z)+"|"
+        newstr = newstr[:-1]
+        df_link['shapeInfo'][idx] = newstr
+        idx+=1  
+  
     return df_probe, df_link
 
 def converter(latitude, longitude, height):
@@ -74,4 +65,4 @@ df_probe = pd.read_csv(probe_path,names=["sampleID", "dateTime", "sourceCode", "
 df_link = pd.read_csv(link_path,names=["linkPVID", "refNodeID", "nrefNodeID", "length", "functionalClass", "directionOfTravel", "speedCategory", "fromRefSpeedLimit", "toRefSpeedLimit", "fromRefNumLanes", "toRefNumLanes", "multiDigitized", "urban", "timeZone", "shapeInfo", "curvatureInfo", "slopeInfo"])
 df_probe, df_link = preprocess(df_probe,df_link)
 df_probe.to_csv('data/new_probe_data.csv', index=False)
-# df_link.to_csv('data/new_link_data.csv', index=False)
+df_link.to_csv('data/new_link_data.csv', index=False)
